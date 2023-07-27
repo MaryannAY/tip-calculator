a7f03af9cd5fc1fe39da85252f6c2c8360edf606
MaryannAY <maryann.ahmed2@gmail.com>
2023-07-27 16:46:12 -0700

    Answer to tip-calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,8 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const bill=400
+if (bill >= 300 || bill <= 400){
+    console.log("The bill was 400, The tip was 80 and total is 480")
+} else if (bill <= 300 || bill >= 50){
+    console.log("The bill was 400, The tip was 80 and total is 480")
+} else console.log(`$(bill) the bill was 400,the tip was 80 and the total was 480`)
